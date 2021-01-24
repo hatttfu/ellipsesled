170d32bedfe3beed6a719c5c0a021dabc113a940
galiya <gkey-587@mail.ru>
2021-01-24 21:14:00 +0500

    hghghghhg

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,45 @@ $(document).ready(function() {
     })
 })
 
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  };
+
+
+
+
+
+$(document).ready(function($) {
+
+    $("#phone3").click(function(){
+        $(this).setCursorPosition(3);
+      }).mask("7 (999) 999 99 99" , {
+        placeholder: "7 (xxx) xxx xx xx"
+    });
+
+    $("#phone3").inputmask({removeMaskOnSubmit: true});
+
+    // const button = document.querySelector('.button');
+
+    // button.onclick = function() {
+    //     location.reload();
+    //     $("#phone3").unmask();
+    // }
+
+     $("#myForm").submit(function() {
+        $("#phone3").unmask();
+       });
+
+});
+
+
+
+
